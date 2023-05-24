f9b2c11de5c0ceea0a92a15aa89d209827cd3f08
ckm66 <stephen041002@gmail.com>
2023-05-25 01:36:25 +0800

    inital Commit

---
+++ survey_analytics_tool/survey_analytics_tool/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def movies(request):
+    return render(request, 'movies/movies.html')
+
+def home(request):
+    return HttpResponse("Welcome to the home page")
